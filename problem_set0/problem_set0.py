0b02d89819ae49e78068b6f4531ca7cb5968a11e
cbuscaron <cbuscaron@gmail.com>
2014-10-02 09:10:13 -0400

    Practice

--- problem_set0/problem_set0.py
+++ problem_set0/problem_set0.py
@@ -5,7 +5,43 @@ Part 1: Write a function that returns the string "hello, world"
 '''
 
 def hello_world():
-	return "hello, world"
+    
+    title = "how to find a circle's area"
+    pi = 3.14159
+    radius = 5
+    area= pi*(radius**2)
+    print title
+    print area
+    
+    x = 5
+    if x > 0:
+        print "True"
+    else:
+        print "False"
+    
+    n = 5;
+    
+    while n > 0:
+        print n
+        n = n -1
+    print "blastoff!"
+    
+    x=5
+    
+    for i in range(x):
+        print i
+        
+        x = 10
+        
+        
+    mylist= [1,2,3]
+    
+    print mylist
+    
+    name = 'Matilda'
+    print name[2:]
+    
+    return "hello, world"
 
 
 
